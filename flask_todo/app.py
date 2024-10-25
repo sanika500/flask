ebd4a0f131ee9e99c1c6796f30664465b3989c7e
Sanika500 <sanikasanthosh500@gmail.com>
2024-10-25 08:47:36 +0530

    done

--- flask_todo/app.py
+++ flask_todo/app.py
@@ -1,36 +1,52 @@
-from flask import Flask,render_template,request,redirect,url_for
 
-app=Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-tasks = []
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+    def _repr_(self):
+        return f'<Todo {self.id} - {self.content}>'
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html',tasks=tasks)
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
 
-@app.route('/add',methods=['POST'])
-def add_task():
-   task=request.form.get('task')
-   if task:
-       tasks.append(task)
-   return redirect(url_for('index'))
-
-@app.route('/delete/<int:task_id>')
-def delete_task(task_id):
-    if 0 <=task_id <len(tasks):
-        tasks.pop(task_id)
+@app.route('/add', methods=['POST'])
+def add_todo():
+    todo_content = request.form.get('content')
+    if todo_content:
+        new_todo = Todo(content=todo_content)
+        db.session.add(new_todo)
+        db.session.commit()
     return redirect(url_for('index'))
 
-@app.route('/edit/<int:task_id>',methods=['POST','get'])
-def edit_task(task_id):
-    if request.method=="POST":
-        update= request.form.get('update')
-        tasks.edit(task_id, update) 
+@app.route('/delete/<int:todo_id>')
+def delete_todo(todo_id):
+    todo_to_delete = Todo.query.get_or_404(todo_id)
+    db.session.delete(todo_to_delete)
+    db.session.commit()
     return redirect(url_for('index'))
 
+@app.route('/edit/<int:todo_id>', methods=['GET', 'POST'])
+def edit_todo(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    if request.method == 'POST':
+        todo.content = request.form.get('content')
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', todo=todo)
 
-if __name__=='__main__':
-    app.run(debug=True)
-
-
+if __name__ == '_main_':
+    app.run(debug=True)     
 
